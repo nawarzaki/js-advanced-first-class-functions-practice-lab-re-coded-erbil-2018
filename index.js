ddc3c38a557282bf0d01388887745408a320fc2d
nawaar.zakee <nawaar.zakee@yahoo.com>
2019-01-24 14:35:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+
+const callBack = function (drivers) {
+  console.log(drivers.name)
+
+}
+function logDriverNames(drivers) {
+  drivers.forEach(callBack)
+}
+
+
+
+function logDriversByHometown(drivers, hometown) {
+
+  let x = drivers.forEach(function (c) {
+  let y =''
+    if (hometown===c.hometown){
+    y = c.name
+    }
+console.log( y);
+  })
+  console.log(x);
+}
+
+function driversByRevenue(drivers) {
+  let z = [...drivers].sort(function(c1, c2){
+    return c1.revenue - c2.revenue
+  })
+
+  return z
+}
